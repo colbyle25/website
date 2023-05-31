6ba31f132549a6a6eb02c1a52b0ffab4068025be
colbyle25 <112435704+colbyle25@users.noreply.github.com>
2023-05-31 17:24:35 -0400

    trying to sync vscode with github

---
+++ src/emailsignup.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default class Emailsignup extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+    return(
+        <div>
+            <h1>test</h1>
+            <button className='btn btn-primary'>hi</button>
+        </div>
+    )
+    }
+}
--- src/socialmedia.js
+++
@@ -1,7 +0,0 @@
-class socialmedia extends React.Component{
-    return(){
-        <h1>test</h1>
-    }
-}
-
-export default socialmedia;
