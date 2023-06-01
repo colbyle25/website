6f64ece3b0f02abe832a3b27d6e60f158fad27c6
colbyle25 <112435704+colbyle25@users.noreply.github.com>
2023-05-31 22:48:48 -0400

    Revert "trying to sync vscode with github"
    This reverts commit 6ba31f132549a6a6eb02c1a52b0ffab4068025be.

--- src/emailsignup.js
+++ src/emailsignup.js
@@ -6,11 +6,24 @@ export default class Emailsignup extends Component{
         super(props)
     }
     render(){
-    return(
-        <div>
-            <h1>test</h1>
-            <button className='btn btn-primary'>hi</button>
-        </div>
-    )
+        return(
+            <div>
+                <form>
+                    <div className = 'row'>
+                        <div className = 'col-md-2'>
+                            <label>Subscribe to This Week in OYFA (WIO)!</label>
+                        </div>
+                        <div className = 'col-md-2'>
+                            <input type = 'email' name = 'emailInput' /> {/* add width to style later */}
+                        </div>
+                        <div className = 'col-md-2'>
+                            <button type = 'submit' name = 'submitBtn' className = 'btn btn-primary'>
+                                <i className ='fa fa-envelope' aria-hidden='false'/> Submit
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )    
     }
 }
