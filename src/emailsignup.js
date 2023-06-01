b9c7101f44c389c705838e3528d0b176c9062a20
colbyle25 <112435704+colbyle25@users.noreply.github.com>
2023-06-01 01:23:53 -0400

    initialized emailsignup component

--- src/emailsignup.js
+++ src/emailsignup.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+//ideas: update the home page to have an image carousel, have a historic pictures showcase, new about us section with large stats/awards details, bulletin section with news/postings
 
 export default class Emailsignup extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            emailInput: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event){
+        this.setState({
+            emailInput: event.target.value
+        })
+    }
+    handleSubmit(event){ // adding behavior later. should add email to WIO mailing list
+        event.preventDefault()
+        alert('Check your email every Sunday for the WIO!' + this.state.emailInput)
+        
     }
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}> {/* call handleSubmit upon submit button pressed */}
                     <div className = 'row'>
                         <div className = 'col-md-2'>
                             <label>Subscribe to This Week in OYFA (WIO)!</label>
                         </div>
                         <div className = 'col-md-2'>
-                            <input type = 'email' name = 'emailInput' /> {/* add width to style later */}
-                        </div>
-                        <div className = 'col-md-2'>
+                            <input type = 'email' value={this.state.emailInput} onChange = {this.handleChange}/> {/* textField updates state variable upon change . add width to style later */}
                             <button type = 'submit' name = 'submitBtn' className = 'btn btn-primary'>
-                                <i className ='fa fa-envelope' aria-hidden='false'/> Submit
+                                Submit{' '}<FontAwesomeIcon icon={faEnvelope}/> {/* submit button with an icon spanning. {' '} is to create a whitespace for a little distance between text and icon */}
                             </button>
                         </div>
                     </div>
