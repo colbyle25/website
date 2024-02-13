4e12e73cb2a95db4dff049f6fa355f339825d77b
Seborromeo <uwg3xs@virginia.edu>
2024-02-13 13:16:15 -0500

    created RootRouteComponent

---
+++ src/root.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+function RootRouteComponent() {
+    return (
+        <>
+            <Outlet />
+        </>
+    );
+}
+
+export default RootRouteComponent;
